ea143e4e4bd1f77bd7ff0c463f857668b9ae2df8
sultonza24 <6110110107@psu.ac.th>
2020-08-13 21:39:06 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <WordCard value={word}/>
     </div>
-  );
+    );
 }
 
 export default App;
